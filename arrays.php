98b5303920c169a4f66133b95a25e21e1f8aef4f
Phil <gentrs80@gmail.com>
2017-12-27 21:38:20 -0800

    PHP Front to Back Part 4 init

---
+++ arrays.php
@@ -0,0 +1,16 @@
+<?php
+
+#Arrays - Variables that hold multiple values
+
+/*
+    - Indexed
+    - Associate
+    - Mult-Dimensional
+
+*/
+
+// Indexed
+$testarry = array('Kevin','Jeremy','Sarah');
+  echo $testarry[1];
+
+?>
