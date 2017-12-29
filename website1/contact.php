d9ed0739ef6c6ef6224f375f98e6b2e9ec6c64e7
Phil <gentrs80@gmail.com>
2017-12-28 23:04:48 -0800

    PHP Front to Back - Part 9 - Include & Require

---
+++ website1/contact.php
@@ -0,0 +1,5 @@
+<!-- Require means that if it fails then the whole process page fails -->
+
+<?php require'inc/header.php'; ?>
+  <h1>Contact</h1>
+<?php require 'inc/footer.php'; ?>
---
+++ website1/index.php
@@ -0,0 +1,6 @@
+<!-- Include means that if the include section fails then only the include will fail and not the whole page fails NOTE: Require will make the whole page fail-->
+
+<?php include('inc/header.php'); ?>
+  <h1>Home</h1>
+<?php include('inc/footer.php'); ?>
+
