0268a1cfa691e9598916d05cbfa4c1eb06e0c35c
Phil <gentrs80@gmail.com>
2017-12-30 18:32:41 -0800

    PHP Front to Back - Part 13 Shorthand Syntax

---
+++ shorthands.php
@@ -0,0 +1,34 @@
+<?php
+
+ $loggedIn = false;
+
+//  if($loggedIn){
+//    echo 'You are logged in';
+//  } else{
+//    echo 'You are NOT logged in';
+//  }
+
+// 
+// #Shorthand  conditional if statement
+
+//  echo ($loggedIn) ? 'You ARE logged in' : 'You are NOT logged in';
+
+//  $isRegistered = ($loggedIn == true) ? true : false;
+//  echo $isRegistered;
+
+// # Shorthand conditional nested statements
+//  $age=8;
+//  $score=12;
+
+//  echo 'Your score is: ' .($score >10 ? ($age > 10 ? 'Average':'Exceptional'):($age > 10 ? 'Horriable':'Average'));
+?>
+
+<!--Shorthand conditional statement with Embedded in HTML -->
+
+<div>
+  <?php if($loggedIn) { ?>
+    <h1>Welcome User</h1>
+ <?php } else {?>
+  <h1>Welcome Guest</h1>
+ <?php } ?>
+</div>
