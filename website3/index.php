85d4085bd36f70fa0d6642e78edd4f7eaf4b6a74
Phil <gentrs80@gmail.com>
2018-01-01 10:42:22 -0800

    PHP Front to Back - Part 15 Contact Form init

---
+++ website3/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Contact Us</title>
+  <link rel="stylesheet" href="https://bootswatch.com/4/cosmo/bootstrap.min.css">
+</head>
+<body>
+  <nav class="nav navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="index.php" class="navbar-brand">My Website</a>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <form action="" method="post">
+      <div class="form-group">
+        <label>Name</label>
+        <input type="text" name="name" class="form-control" value="">
+      </div>
+      <div class="form-group">
+        <label>Email</label>
+        <input type="email" name="email" id="" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Message</label>
+        <textarea name="message" i class="form-control"></textarea>
+      </div>
+      <br>
+      <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</body>
+</html>
