7c7778849e03aafbdfcc947dc2d24f2b9aa93f5e
Phil <gentrs80@gmail.com>
2018-01-01 21:27:16 -0800

    PHP Front to Back - Part 15 Contact Form - Validation

--- website3/index.php
+++ website3/index.php
@@ -1,3 +1,47 @@
+<?php
+
+  // Message Variables
+    $msg ='';
+    $msgClass ='';
+
+
+  # Check for submit
+  if(filter_has_var(INPUT_POST,'submit')){
+    // echo 'Submitted';
+
+    # Get form Data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    # Check Required fields
+    if(!empty($name) && !empty($email) && !empty($message)){
+      // # Passed -- Not empty
+      // echo 'PASSED';
+
+      # Validate email, IF NOT valid then TRUE
+      if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+
+          # Passed
+          echo 'EMAIL PASSED';       
+      } else {
+
+        # Failed
+        echo 'FAILED EMAIL';   
+        $msg='Please use a valid email addres';
+        $msgClass = 'alert-danger';
+      }
+
+
+    }else{
+      //Failed
+      $msg='Please fill in all the field';
+      $msgClass = 'alert-danger';
+    }
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,23 +55,27 @@
   <nav class="nav navbar-default">
     <div class="container">
       <div class="navbar-header">
-        <a href="index.php" class="navbar-brand">My Website</a>
+        <a class="navbar-brand" href="index.php">My Website</a>
       </div>
     </div>
   </nav>
   <div class="container">
-    <form action="" method="post">
+    <?php if($msg != ''): ?>
+      <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
+    <?php endif; ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
       <div class="form-group">
         <label>Name</label>
-        <input type="text" name="name" class="form-control" value="">
+        <input name="name" type="text" class="form-control" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
       </div>
       <div class="form-group">
         <label>Email</label>
-        <input type="email" name="email" id="" class="form-control">
+        <!-- NOTE: type=textneeds to be text for email NOTE type=email -->
+        <input name="email" type="text" class="form-control" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
       </div>
       <div class="form-group">
         <label>Message</label>
-        <textarea name="message" i class="form-control"></textarea>
+        <textarea name="message" class="form-control"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
       </div>
       <br>
       <button type="submit" name="submit" class="btn btn-primary">Submit</button>
