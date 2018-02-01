1e98221d0dc8859c726a6b9f05fe1728d1ff5fd5
Phil <gentrs80@gmail.com>
2018-02-01 11:41:40 -0800

    PHP Front to Back - Part 13 Shorthand Syntax - for and foreach loop

--- shorthands.php
+++ shorthands.php
@@ -2,6 +2,8 @@
 
  $loggedIn = false;
 
+ $arr=[1,2,3,4,5];
+
 //  if($loggedIn){
 //    echo 'You are logged in';
 //  } else{
@@ -38,5 +40,20 @@
 <div>
   <?php if($loggedIn): ?>
     <h1>Welcome User</h1>
+ <?php else: ?>
+ <h1>Welcome Guest</h1>
  <?php endif; ?>
+</div>
+
+<div>
+  <?php foreach ($arr as $val): ?>
+    <?php echo $val; ?>
+    <br>
+ <?php endforeach; ?>
+</div>
+
+<div>
+<?php for($i=0;$i<10;$i++): ?>
+  <li><?php echo $i; ?><li>
+ <?php endfor; ?> 
 </div>
