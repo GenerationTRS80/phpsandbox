66c372d93110d7be2aed961dd5688fea4bb9576c
Phil <gentrs80@gmail.com>
2018-02-02 15:43:16 -0800

    PHP Front to Back - Part 15 Contact Form - Validation Feb 02 18

--- website3/index.php
+++ website3/index.php
@@ -10,24 +10,26 @@
     // echo 'Submitted';
 
     # Get form Data
+    # NOTE: Use htmlspecialchars() to prevent the running of malicious code
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    # Check Required fields
+    # Check Required fields 1) NOT Empty Name, Email, Message
     if(!empty($name) && !empty($email) && !empty($message)){
-      // # Passed -- Not empty
-      // echo 'PASSED';
 
-      # Validate email, IF NOT valid then TRUE
+      // # Passed -- Not empty
+
+      # Validate EMAIL, IF NOT valid then TRUE
       if(filter_var($email,FILTER_VALIDATE_EMAIL)){
 
           # Passed
           // echo 'EMAIL PASSED';  
-          # Recipient Email
+          # Set up Recipient Email
           $toEmail = 'phil@pseiersen.com';
           $subject = "Contact request from $name";
-          $body = '<h2>Contact Request</h2>
+          $body = '
+              <h2>Contact Request</h2>
               <h4>Name</h4><p>"$name"</p>
               <h4>Email</h4><p>"$Email"</p>
               <h4>Message</h4><p>"$message"</p>
@@ -50,6 +52,7 @@
               $msgClass = 'alert-success';           
           } else {
 
+            // FAIL to send msg
               echo 'EMAIL NOT Sent';   
               $msg='Your email was not sent';
               $msgClass = 'alert-danger';          
@@ -77,9 +80,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Contact Us</title>
   <link rel="stylesheet" href="https://bootswatch.com/4/cosmo/bootstrap.min.css">
 </head>
@@ -92,21 +92,33 @@
     </div>
   </nav>
   <div class="container">
+
+  <!-- Show Error Msg below -->
     <?php if($msg != ''): ?>
       <div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div>
     <?php endif; ?>
+
+    <!-- Form begins here -->
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
       <div class="form-group">
         <label>Name</label>
+
+        <!-- Make the form retain the values inputted by using isset($_POST['name']) -->
         <input name="name" type="text" class="form-control" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
+
       </div>
       <div class="form-group">
         <label>Email</label>
+
         <!-- NOTE: type=textneeds to be text for email NOTE type=email -->
+        <!-- Make the form retain the values inputted by using isset($_POST['email']) -->
         <input name="email" type="text" class="form-control" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
+
       </div>
       <div class="form-group">
         <label>Message</label>
+
+        <!-- Make the form retain the values inputted by using isset($_POST['message']) -->        
         <textarea name="message" class="form-control"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
       </div>
       <br>
