b856ff8817a9fd0f429ececa068e41dcdb0bd771
Phil <gentrs80@gmail.com>
2018-02-04 23:02:00 -0800

    PHP Front To Back [Part 17] Cookies - Completed

--- website5/page2.php
+++ website5/page2.php
@@ -4,10 +4,24 @@
 
     setcookie('username','Frank',time() + (86400 * 30));
 
-    // To unset the cookie above you need to set the time in the past
-    // Note: -3600 set time back an hour
-    setcookie('username','Frank',time() - 3600);
+    // // To unset or Delete the cookie above you need to set the time in the past
+    // // Note: -3600 set time back an hour
+    // setcookie('username','Frank',time() - 3600);
 
+    // Check for set cookies
+    $countK=count($_COOKIE);
+
+    if($countk=1){
+        echo 'There is ' .count($_COOKIE). ' cookie saved <br>';
+
+    } elseif($countK>0) {
+        echo 'There is ' .count($_COOKIE). ' cookie saved <br>';
+
+    } else {
+        echo 'There are no cookies saved <br>';
+    }
+
+    
     if(isset($_COOKIE['username'])){
       echo 'User '. $_COOKIE['username'] . ' is set <br>';
 
