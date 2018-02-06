8acf224cf35a50dea699e482e15b931111f44c7d
Phil <gentrs80@gmail.com>
2018-02-05 16:58:04 -0800

    PHP Front To Back [Part 18] PHP & AJAX - Feb 05 18

--- website6/index.php
+++ website6/index.php
@@ -1,8 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Search User</title>
+  <title>PHP - Ajax/JSscript Search User</title>
   <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css">
+  <script>
+      function showSuggestion(sString){
+        if(sString.length == 0) {
+          
+          document.getElementById('output').innerHTML = '';
+        
+        } else {
+          
+          // Make a get request to a page using AJAX's  http object xmlhttp
+          var xmlhttp = new XMLHttpRequest();
+          xmlhttp.onreadystatechange = function(){
+
+            console.log(sString);
+
+            // readystate = 4 request finished and response is ready -- Status = 200 "OK"
+            if (this.readyState == 4 && this.status == 200) {
+
+              document.getElementById('output').innerHTML = this.responseText;
+
+            } 
+          }
+
+          xmlhttp.open("GET", "suggest.php?q="+sString, true);
+          xmlhttp.send();
+
+        }
+      }
+  </script>
   
 </head>
 <body>
---
+++ website6/suggest.php
@@ -0,0 +1,51 @@
+<?php
+  // People Array
+  $people[]= "Steve";
+  $people[]= "John";
+  $people[]= "Kathy";
+  $people[]= "Evan"; 
+  $people[]= "Anthony";  
+  $people[]= "Tom";   
+  $people[]= "Rhonda";     
+  $people[]= "Hal";     
+  $people[]= "Ernie";    
+  $people[]= "Johanna"; 
+  $people[]= "Farrah"; 
+  $people[]= "Linda"; 
+  $people[]= "Shawn"; 
+  $people[]= "Oliva"; 
+  $people[]= "Derek";
+  
+  // Get Query String
+  $sQuery = $_REQUEST['q'];
+
+  // Set default value as suggestions
+  $suggestion = "";
+
+  // Get Suggestions 
+  if($sQuery !== ""){
+      $Query = strlower($sQuery);
+      $iLen = strlen($sQuery);
+
+      foreach($people as $person){
+        // stristr = Find the first occurance in $sQuery  
+        if(stristr($sQuery, substr($person,0,$iLen))){
+
+          //  === is equal too
+          if($suggestion === ""){
+              $suggestion = $person;
+
+          } else {
+              $suggestion .= ", $person";
+
+          }
+
+        }
+      }
+
+  }
+
+// IF there is NOT a Suggestion ($suggestion ==== "" then (?) say "No suggestion" else (:) say $suggestion
+echo $suggestion === "" ? "No Suggestion" : $suggestion;
+
+?>
