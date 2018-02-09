3850c031072569cdb473a999749bc44c4cf8ab68
Phil <gentrs80@gmail.com>
2018-02-08 16:12:32 -0800

    PHP Front To Back [Part 19] File system functions - init

---
+++ website7/index.php
@@ -0,0 +1,67 @@
+<?php
+
+
+    $path='/dir0/dir/myfile.php';
+    $file= 'file1.txt';
+
+/* # File info
+    # Return filename
+    $filename = basename($path);
+    $dirname = dirname($path);
+
+    # You can use file_exist for both a file and folder
+    $fileexists = file_exists($file);
+
+    # Get abs path
+    $realpath = realpath($file);
+
+    # is_file you can use to just check for a file and not a folder
+    $isfile = is_file($file);
+
+    # is file writeable
+    $iswriteable = is_writable($file);
+    $isreadable = is_readable($file);
+
+    echo "The filename = $filename <br> The directory name = $dirname <br> File exists = $fileexists";
+    echo "<br>The realpath = $realpath <br> Is file=$isfile <br> Is writeable = $iswriteable <br> Is readable = $isreadable";
+ */
+
+/* # Manipulate file and directory
+  # Create Directory
+  # mkdir('testing');
+
+  # Remove directory
+  # rmdir('testing');
+
+  # Copy file
+  # echo copy('file1.txt','file2.txt');
+
+  # rename file
+  # rename('file2.txt','myfile.txt');
+
+  # Delete file
+  # unlink('myfile.txt');
+
+*/
+
+
+/* # read and write file contents to page
+
+# Write to file
+# echo file_put_contents($file, 'Hello World');
+
+# Write from file to string
+# echo file_get_contents($file);
+
+*/
+
+#Append contents
+$current = file_get_contents($file);
+
+$current .= ' Goodby World!';
+
+file_put_contents($file,$current);
+
+# Put a file into a variable
+
+?>
