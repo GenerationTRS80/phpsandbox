829251e703a229982d53e89caef5aa9a3fc33e2f
Phil <gentrs80@gmail.com>
2018-04-07 15:51:23 -0700

    phpblog post - Edit post completed

--- website8/editpost.php
+++ website8/editpost.php
@@ -7,10 +7,13 @@
   require('config/config.php'); 
   require('config/connectDB.php');
 
+
+
   // Check for submint
   if(isset($_POST['submit'])){
       // Get form data
-      $udpate_id== mysqli_real_escape_string($conn,$_POST['update_id']);
+      // $udpate_id= $id;
+      $udpate_id= mysqli_real_escape_string($conn,$_POST['update_id']);
       $title = mysqli_real_escape_string($conn,$_POST['title']);
       $body = mysqli_real_escape_string($conn,$_POST['body']);
       $author = mysqli_real_escape_string($conn,$_POST['author']);
@@ -20,8 +23,10 @@
           title='$title',
           body='$body',
           author='$author'
-            WHERE id = {$udpate_id}";
+            WHERE posts_id = {$udpate_id}";
 
+      // >> Show query text <<
+      // die($query);
 
       if(mysqli_query($conn,$query)){
         header('Location: '.ROOT_URL.'');
@@ -31,12 +36,11 @@
 
   }
 
-   // get ID  << Get the current post selected with a query
+  // get ID  << Get the current post selected with a query
   if (isset($_GET['posts_id']))
   {
     $id=mysqli_real_escape_string($conn,$_GET['posts_id']);
   }
-
 
   // Create query with where clause 
   $query= 'Select * FROM posts WHERE posts_id = '.$id;
@@ -77,18 +81,18 @@
         <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
           <div class="form-group">
             <label>Title</label>
-            <input type="text" name="title" class="form-control" value=<?php echo $post['title']; ?>>
+            <input type="text" name="title" class="form-control" value="<?php echo $post['title']; ?>">
           </div>
           <div class="form-group">
             <label>Author</label>
-            <input type="text" name="author" class="form-control" value=<?php echo $post['author']; ?>>
+            <input type="text" name="author" class="form-control" value="<?php echo $post['author']; ?>">
           </div>
           <div class="form-group">
             <label>Body</label>
             <textarea name="body" class="form-control"><?php echo $post['body']; ?></textarea>
           </div>
           <!-- Hidden value -->
-          <input type=hidden name="update_id" value="<?php echo $post['posts_id']; ?>">
+          <input type=hidden name="update_id" value=<?php echo $post['posts_id']; ?>>
           <input type="submit" name="submit" value="Submit" class="btn btn-primary">
         </form>
       </div>
