c7a89ea066d0b3494f2b5d6c783ed6c0d3727ce2
Phil <gentrs80@gmail.com>
2018-04-07 17:57:28 -0700

    phpblog post - Delete post wError

--- website8/post.php
+++ website8/post.php
@@ -7,6 +7,26 @@
   require('config/config.php'); 
   require('config/connectDB.php');
 
+
+  // Check for DELETE submit 
+  if(isset($_POST['delete'])){
+
+    // Delete post where posts_id = delete_id
+    $delete_id= mysqli_real_escape_string($conn,$_POST['delete_id']);
+
+    echo "DeleteID = $delete_id<br>";
+
+    // Database transaction: insert into DB table POSTS
+    $query="DELETE FROM posts WHERE posts_id = {$delete_id}";
+
+    if(mysqli_query($conn,$query)){
+      header('Location: '.ROOT_URL.'');
+    } else {
+      echo 'ERROR: '. mysqli_error($conn);
+    }
+
+  }
+
   // get ID  << Get the current post selected with a query
   # Remove dangerous charachter  mysqli_real_escape_string()
   # https://phillipnb.wordpress.com/2010/11/26/undefined-index-and-_get/
@@ -32,6 +52,7 @@
   // # Show what is in $posts object as string
   // var_dump($posts);  
 
+
   // Free results - Close recordset
   mysqli_free_result($result);
 
@@ -50,7 +71,13 @@
         <?php echo $posts['author']; ?></small>
         <p><?php echo $posts['body'];?></p>
         <hr>
+
+        <!-- Delete post -->
+        <form class="pull-right" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+          <input type="hidden" name="delete_id" value="<?php echo $post['posts_id']; ?>">
+          <input type="submit" name="delete" value="Delete" class="btn btn-danger">
+        </form>
         <!-- Fixed reference to editpost by removing "" from href=""-->
         <a class="btn btn-default" style="background-color: #d9d9d9" href=<?php echo ROOT_URL;?>editpost.php?posts_id=<?php echo $posts['posts_id']; ?>>Edit Post</a>
-      </div>
+        </div>
     <?php include('inc/footer.php'); ?>
